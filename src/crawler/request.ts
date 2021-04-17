116a52497ed7e7efd009c472d1d652116d1a1e95
DefectingCat <i@defect.ink>
2021-04-17 11:00:44 +0800

    rm

--- src/crawler/request.ts
+++ src/crawler/request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 const request: AxiosInstance = axios.create({
-  baseURL: 'http://152.136.185.210:7878/api/m5',
+  baseURL: '',
   timeout: 1000000,
 });
 request.interceptors.response.use((value) => {
